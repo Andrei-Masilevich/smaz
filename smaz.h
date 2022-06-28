27cd21bca53b34425fbeb1a9890e75ceadf1a54a
Andrei Masilevich <a.masilevich@yandex.ru>
2022-06-28 19:04:52 +0300

    Formatted

--- smaz.h
+++ smaz.h
@@ -1,7 +1,17 @@
 #ifndef _SMAZ_H
 #define _SMAZ_H
 
-int smaz_compress(char *in, int inlen, char *out, int outlen);
-int smaz_decompress(char *in, int inlen, char *out, int outlen);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int MAX_LEN_ONE_SYMBOL_ENCODED;
+
+int smaz_compress(char* in, int inlen, char* out, int outlen);
+int smaz_decompress(char* in, int inlen, char* out, int outlen);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
